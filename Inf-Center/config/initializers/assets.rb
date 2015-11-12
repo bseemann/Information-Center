1465a237b0c0cdc11858a2f1f591779fd73845dc
Mauro Victor <mauro.victor5@gmail.com>
2015-11-12 20:24:53 -0200

    basics of files

--- Inf-Center/config/initializers/assets.rb
+++ Inf-Center/config/initializers/assets.rb
@@ -11,3 +11,6 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( main.css )
 Rails.application.config.assets.precompile += %w( modernizr-2.8.3-respond-1.4.2.min.js )
+Rails.application.config.assets.precompile += %w( material.min.js )
+Rails.application.config.assets.precompile += %w( jquery-2.1.4.min.js )
+Rails.application.config.assets.precompile += %w( page.js )
